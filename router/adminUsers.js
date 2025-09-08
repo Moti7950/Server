4ca0e74d05094223cc62c87b56c819c2a25ae651
Moti7950 <shalom7950@gmail.com>
2025-09-08 09:58:50 +0300

    All end point work I checked the workers by Log

--- router/adminUsers.js
+++ router/adminUsers.js
@@ -3,22 +3,28 @@ import express from "express"
 // this user is an admin user web
 const adminUsers = express.Router();
 
+// work with curl -X DELETE  http://127.0.0.1:9085/api/admin/deletePost/:1
 // this api for delete a post
-adminUsers.delete("deletePost/:id", (req, res) => {
+adminUsers.delete("/deletePost/:id", (req, res) => {
+    const { id } = req.params || {};
     console.log("Hi from admin! user on route delete");
 
 })
 
+// work with curl -X POST http://127.0.0.1:9085/api/admin/create/post
 // this api for create a post
 adminUsers.post("/create/post", (req, res) => {
     console.log("Hi from admin! user on route create post");
 })
 
+//work with curl -X PUT http://127.0.0.1:9085/api/admin/update/post/:2
 // this api for update a post
 adminUsers.put("/update/post/:id", (req, res) => {
+    const { id } = req.params || {};
     console.log("Hi from admin! user on route update post");
 })
 
+// work with curl -X GET http://127.0.0.1:9085/api/admin/show/posts
 // this api for show all post
 adminUsers.get("/show/posts", (req, res) => {
     console.log("Hi from admin! user on route show all posts");
--- router/users.js
+++ router/users.js
@@ -3,22 +3,28 @@ import express from "express"
 // this user is not an admin user (admin web)
 const users = express.Router();
 
+// work with curl -X DELETE  http://127.0.0.1:9085/api/users/deletePost/:1
 // this api for delete a post
-users.delete("deletePost/:id", (req, res)=>{
-    console.log("Hi from admin! user on route delete");
-    // const {id} = req.params || {}
+users.delete("/deletePost/:id", (req, res)=>{
+    const {id} = req.params || {};
+    console.log("Hi from: user on route delete",id);
 })
 
+// work with curl -X POST http://127.0.0.1:9085/api/users/create/post
 // this api for create a post
 users.post("/create/post",(req, res)=>{
     console.log("Hi from user on route create post");
 })
 
+// work with curl -X PUT http://127.0.0.1:9085/api/users/update/post/:2
 // this api for update a post
 users.put("/update/post/:id",(req, res)=>{
-    console.log("Hi from user on route update post");
+    const {id} = req.params || {};
+    console.log("Hi from user on route update post",id);
+
 })
 
+// Work with curl -X GET http://127.0.0.1:9085/api/users/show/posts
 // this api for show all post
 users.get("/show/posts",(req, res)=>{
     console.log("Hi from user on route show all posts");
