b49df517de0b190cb8f57ef7f03dc354db7cea57
Moti7950 <shalom7950@gmail.com>
2025-09-08 09:28:49 +0300

    I finish write the end point only, And i added log conection

--- router/adminUsers.js
+++ router/adminUsers.js
@@ -4,15 +4,24 @@ import express from "express"
 const adminUsers = express.Router();
 
 // this api for delete a post
-adminUsers.delete()
+adminUsers.delete("deletePost/:id", (req, res) => {
+    console.log("Hi from admin! user on route delete");
+
+})
 
 // this api for create a post
-adminUsers.post()
+adminUsers.post("/create/post", (req, res) => {
+    console.log("Hi from admin! user on route create post");
+})
 
 // this api for update a post
-adminUsers.put()
+adminUsers.put("/update/post/:id", (req, res) => {
+    console.log("Hi from admin! user on route update post");
+})
 
 // this api for show all post
-adminUsers.get()
+adminUsers.get("/show/posts", (req, res) => {
+    console.log("Hi from admin! user on route show all posts");
+})
 
 export default adminUsers;
--- router/users.js
+++ router/users.js
@@ -4,15 +4,24 @@ import express from "express"
 const users = express.Router();
 
 // this api for delete a post
-users.delete()
+users.delete("deletePost/:id", (req, res)=>{
+    console.log("Hi from admin! user on route delete");
+    // const {id} = req.params || {}
+})
 
 // this api for create a post
-users.post()
+users.post("/create/post",(req, res)=>{
+    console.log("Hi from user on route create post");
+})
 
 // this api for update a post
-users.put()
+users.put("/update/post/:id",(req, res)=>{
+    console.log("Hi from user on route update post");
+})
 
 // this api for show all post
-users.get()
+users.get("/show/posts",(req, res)=>{
+    console.log("Hi from user on route show all posts");
+})
 
 export default users;
